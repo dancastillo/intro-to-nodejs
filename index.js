10d023b0a927807406f269f4f9fdc0cad77419cf
dancastillo <dancastillo99@gmail.com>
2018-05-02 20:53:51 -0400

    index.js: display random robots

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const app = express();
-const uc = require('upper-case');
+const key = require('./key');
+const randomstring = require('randomstring');
+const request = require('request');
+let image = '';
+
+const options = {
+  uri: 'https://robohash.p.mashape.com/index.php?text=' + randomstring.generate(5),
+  headers: {
+    'X-Mashape-Key': key.robohash,
+    'Accept': 'application/json'
+  }
+}
+
+request(options, (error, response, body) => {
+  if (!error && response.statusCode == 200) {
+    body = JSON.parse(body);
+    image = body.imageUrl;
+    console.log(image);
+  }
+});
 
 app.get('/', (req, res) => {
-  res.send(uc('hello node js'));
+  res.send(`<img src="${image}" />`)
 });
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!')
-});
+})
