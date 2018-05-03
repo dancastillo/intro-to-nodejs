3caed730ceba87b2bf8b912b0b9cf4536df8752f
dancastillo <dancastillo99@gmail.com>
2018-05-02 21:04:55 -0400

    key.js: added instructions on getting api key

--- key.js
+++ key.js
@@ -1,3 +1,7 @@
+// to get an api key 
+// visit https://market.mashape.com/blaazetech/robohash-image-generator
+// sign up for an account and recieve your api key
+// then replace 'YOUR_API_KEY' with a real api key
 const robohash = 'YOUR_API_KEY';
 
 module.exports = {robohash};
